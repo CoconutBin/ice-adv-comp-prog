3ff861617720d4879ee0c993e783ee82685c6076
CoconutBin <sea99687@gmail.com>
2026-04-23 01:13:42 +0700

    GameSetup refactored

--- game/setup/GameSetup.java
+++ game/setup/GameSetup.java
@@ -3,28 +3,24 @@ package game.setup;
 import entities.*;
 import entities.observers.*;
 import game.io.IOHandler;
+import game.ui.Visuals;
 
 public class GameSetup {
     private final Player player;
     private final Boss boss;
     private final IOHandler ioHandler;
+    private final Visuals visuals;
 
-    public GameSetup(Player player, Boss boss, IOHandler ioHandler) {
+    public GameSetup(Player player, Boss boss, IOHandler ioHandler, Visuals visuals) {
         this.player = player;
         this.boss = boss;
         this.ioHandler = ioHandler;
+        this.visuals = visuals;
     }
 
-    /**
-     * Registers all observers with entities (Observer pattern)
-     */
     public void setupObservers() {
-        // Logger: logs all HP changes for debugging/UI
-        player.addObserver(new EntityLoggerObserver(new game.ui.Visuals(ioHandler), player));
-        boss.addObserver(new EntityLoggerObserver(new game.ui.Visuals(ioHandler), boss));
-
-        // Behavior: switches boss attack strategy based on HP %
-        BossBehaviorObserver behaviorObserver = new BossBehaviorObserver(ioHandler, boss);
-        boss.addBossObserver(behaviorObserver);
+        player.addObserver(new EntityLoggerObserver(visuals, true));
+        boss.addObserver(new EntityLoggerObserver(visuals, false));
+        boss.addObserver(new BossBehaviorObserver(ioHandler, boss));
     }
 }
